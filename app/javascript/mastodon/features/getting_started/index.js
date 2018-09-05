a5d5de5a80b216e4e315a5dde1ffabbeeb5e2bbb
Tristan Mahé <gled@remote-shell.net>
2018-09-05 10:34:43 -0700

    restore trends container

--- app/javascript/mastodon/features/getting_started/index.js
+++ app/javascript/mastodon/features/getting_started/index.js
@@ -154,6 +154,8 @@ class GettingStarted extends ImmutablePureComponent {
             {navItems}
           </div>
 
+          {multiColumn && <TrendsContainer />}
+
           {!multiColumn && <div className='flex-spacer' />}
 
           <div className='getting-started__footer'>
