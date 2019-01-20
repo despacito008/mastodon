00087e4df1bf57601352566d0e70177e2455d2eb
Allen Zhong <allen@moe.cat>
2019-01-20 23:09:47 +0800

    fix messed syntax on coding merging

--- app/javascript/mastodon/features/ui/util/async-components.js
+++ app/javascript/mastodon/features/ui/util/async-components.js
@@ -131,7 +131,8 @@ export function ListEditor () {
 }
 
 export function ListAdder () {
-  return import(/*webpackChunkName: "features/list_adder" */'../../list_adder');
+  return import(/* webpackChunkName: "features/list_adder" */'../../list_adder');
+}
 
 export function Trends () {
   return import(/* webpackChunkName: "features/trends" */'../../trends');
